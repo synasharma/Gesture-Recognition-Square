b34e959a6c37a05209a0184423bf879ad6fed456
synasharma <61623971+synasharma@users.noreply.github.com>
2021-04-24 19:51:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(500,400);
+    canvas.position(1100,250);
+    video=createCapture(500,400);
+    video.position(300,250);
+}
+
+function draw()
+{
+
+}
